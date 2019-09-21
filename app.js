920b44a166d61fc0aa7b10b7aafd6980d533acfb
Savio <savio@Savios-MacBook-Pro.local>
2019-09-21 11:26:27 -0600

    js and html added

---
+++ app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDvw62rtRmshxgAbG1g3e7ZqTF9vA0JgIs",
+    authDomain: "thomasthedankengine-1bf08.firebaseapp.com",
+    databaseURL: "https://thomasthedankengine-1bf08.firebaseio.com",
+    projectId: "thomasthedankengine-1bf08",
+    storageBucket: "",
+    messagingSenderId: "720005099292",
+    appId: "1:720005099292:web:f85ab69e52f89dbaaed4d9"
+  };
+
+  firebase.initializeApp(firebaseConfig);
